0f38b5ec1d22ee33d29dbb31ff43da0688084dc4
Nathaniel Fitch <natetheskate2005@gmail.com>
2026-05-08 15:31:56 -0400

    Add files via upload

---
+++ CS2400/lab1/lab1.cc
@@ -0,0 +1,19 @@
+//* * * * * * * * * * * * * * * * * * * * * * * * * * 
+// 
+// program: <lab1.cc> 
+// 
+// Name : <Nathaniel Fitch>
+// Date : <9/3/2023> 
+// Email : <nf843222@ohio.edu>
+//
+// Description: Below code is used to print a message. Whatever is typed in the quotation marks will be printed in the terminal
+// 
+// * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ 
+#include <iostream>
+using namespace std;
+int main( ) 
+{ // Type whatever I want to be printed
+ cout << "This is my first test at running C++ \nThe software download was a pain Lol \nThanks goes to Nathan Fout for helping me figure out the project\n"; 
+ return (0);
+}
---
+++ CS2400/lab2/lab2.cc
@@ -0,0 +1,22 @@
+//* * * * * * * * * * * * * * * * * * * * * * * * * * 
+// 
+// program: <lab2.cc> 
+// 
+// Name : <Nathaniel Fitch>
+// Date : <9/8/2023> 
+// Email : <nf843222@ohio.edu>
+//
+// Description: 
+// 
+// * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ 
+#include <iostream>
+using namespace std;
+int main( ) {
+
+    
+
+
+
+return(0);
+}
